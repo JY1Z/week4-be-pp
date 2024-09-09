0f9914bc9df8c421e282a94562e64ca9431100bb
jiayuez <Jiayue.Zheng@metropolia.fi>
2024-09-09 15:13:20 +0300

    Integrate morgan

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const express = require('express');
 const app = express();
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
+const morgan = require('morgan');
 
 // Middleware to parse JSON
 app.use(express.json());
 
-// Use the carRouter for all /tours routes
-app.use('/tours', tourRouter);
+// app.use(morgan('dev'));
+// app.use(morgan('short'));
+app.use(morgan('tiny'));
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
-app.use('/users', userRouter);
+app.use('/api/tours', tourRouter);
+app.use('/api/users', userRouter);
+
+
 const port = 4000;
 // Start the server
 app.listen(port, () => {
